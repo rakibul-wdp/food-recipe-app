76d6373ad2bb05761e8c775a410c2fb97b90851e
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-31 19:30:11 +0600

    create review routes

---
+++ server/routes/reviewRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  addReview,
+  deleteReview,
+  getReview,
+  getReviews,
+  updateReview
+} from "../controllers/reviewController.js";
+import requireAuth from "../middleweare/requireAuth.js";
+const router = express.Router({ mergeParams: true });
+
+router.route("/").get(getReviews).post(requireAuth, addReview);
+router
+  .route("/:id")
+  .get(getReview)
+  .put(requireAuth, updateReview)
+  .delete(requireAuth, deleteReview);
+
+export default router;
