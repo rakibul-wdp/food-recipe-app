da6dc581b116cc4bf905e3e0d8056e2e02e1d8ab
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-31 19:26:27 +0600

    create all possible recipe routes

---
+++ server/routes/recipeRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+  createRecipe,
+  deleteRecipe,
+  getRecipe,
+  getRecipes,
+  updateRecipe
+} from "../controllers/recipeController.js";
+import requireAuth from "../middleweare/requireAuth.js";
+
+const router = express.Router({ mergeParams: true });
+
+import reviewRouter from "./reviewRoutes.js";
+router.use("/:recipeId/reviews", reviewRouter);
+
+router.route("/").post(requireAuth, createRecipe).get(getRecipes);
+router
+  .route("/:id")
+  .get(getRecipe)
+  .put(requireAuth, updateRecipe)
+  .delete(requireAuth, deleteRecipe);
+
+export default router;
