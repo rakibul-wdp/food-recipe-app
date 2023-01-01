c1884a662dff3f78e390f9a2c912ac6ebc7dff09
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-01 11:59:06 +0600

    create reviews data

---
+++ server/data/reviews.js
@@ -0,0 +1,28 @@
+const reviews = [
+  {
+    _id: "6207e5f930d0a1963d1db93a",
+    title: "Incredible!",
+    text: "This was quite tasty & easy to make",
+    rating: 5,
+    recipe: "6207d4cdf86c738e71d80f33",
+    user: "6207d95b0c5f4595c2c44394",
+  },
+  {
+    _id: "6207e62a30d0a1963d1db93b",
+    title: "Amazing!",
+    text: "My husband is Turkish and he loved it.. Very easy to make. Will make again and again.\n",
+    rating: 5,
+    recipe: "6207e3f130d0a1963d1db937",
+    user: "6207d95b0c5f4595c2c44394",
+  },
+  {
+    _id: "6207e68430d0a1963d1db93c",
+    title: "Great!",
+    text: "Easy and delicious, couldn't recommend it more!",
+    rating: 5,
+    recipe: "6207e21a30d0a1963d1db933",
+    user: "6207dda430d0a1963d1db92a",
+  },
+];
+
+export default reviews;
