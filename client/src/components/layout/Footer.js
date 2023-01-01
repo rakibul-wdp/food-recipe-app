f168098435d74c4279b620728838569af7443274
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-01 15:20:23 +0600

    create footer layout

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer id="footer" className="footer">
+      <div className="container">
+        <div className="footer__developer">
+          <p>
+            Foodie Guide is a web application developed by{" "}
+            <a href="mailto:nusrathjahan58@gmail.com">
+              <strong>Md. Rakibul Islam</strong>
+            </a>
+          </p>
+          <p>&copy; 2023. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
