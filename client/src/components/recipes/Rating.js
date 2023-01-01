491fb55b9411a18b81b3fed66a4ce81251f66abf
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-01 15:25:25 +0600

    create rating for recipe

---
+++ client/src/components/recipes/Rating.js
@@ -0,0 +1,64 @@
+const Rating = ({ value, numReviews }) => {
+  return (
+    <div className="rating">
+      <span>
+        <i
+          style={styles}
+          className={
+            value >= 1
+              ? "fas fa-star"
+              : value >= 0.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+        <i
+          style={styles}
+          className={
+            value >= 2
+              ? "fas fa-star"
+              : value >= 1.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+        <i
+          style={styles}
+          className={
+            value >= 3
+              ? "fas fa-star"
+              : value >= 2.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+        <i
+          style={styles}
+          className={
+            value >= 4
+              ? "fas fa-star"
+              : value >= 3.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+        <i
+          style={styles}
+          className={
+            value >= 5
+              ? "fas fa-star"
+              : value >= 4.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>{numReviews && `(${numReviews}) `}</span>
+    </div>
+  );
+};
+
+const styles = {
+  color: "#E16120",
+};
+export default Rating;
